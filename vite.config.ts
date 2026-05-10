89cd44443790e1dee67901df565af4b06e36c24c
elPescad <das255@njit.edu>
2026-05-10 15:41:21 -0400

    added ngrok server hosting

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    // Allows the ngrok URL to access the dev server
+    allowedHosts: ['.ngrok-free.app'], 
+    hmr: {
+      // Connect HMR to the secure ngrok tunnel port
+      clientPort: 443, 
+    },
+  },
   plugins: [
     react(),
     tailwindcss(),
